fa5e212b81d9f5963b5b7e6a22e6461db377bd82
SungjungKim <ajax0615@gmail.com>
2015-03-27 13:04:56 +0000

    RECOMM Server

--- src/Bluetooth/RaspberryPi_Bluetooth/rfcomm-server.py
+++ src/Bluetooth/RaspberryPi_Bluetooth/rfcomm-server.py
@@ -1,9 +1,6 @@
 # file: rfcomm-server.py
-# auth: Albert Huang <albert@csail.mit.edu>
 # desc: simple demonstration of a server application that uses RFCOMM sockets
 #
-# $Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
-
 from bluetooth import *
 
 server_sock=BluetoothSocket( RFCOMM )
@@ -12,14 +9,16 @@ server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+uuid = "00001101-0000-1000-8000-00805f9b34fb"
+#uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-advertise_service( server_sock, "SampleServer",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ], 
-#                   protocols = [ OBEX_UUID ] 
-                    )
+advertise_service( 
+	server_sock, "SampleServer",
+	service_id = uuid,
+        service_classes = [ uuid, SERIAL_PORT_CLASS ],
+        profiles = [ SERIAL_PORT_PROFILE ], 
+#       protocols = [ OBEX_UUID ] 
+)
                    
 print "Waiting for connection on RFCOMM channel %d" % port
 
