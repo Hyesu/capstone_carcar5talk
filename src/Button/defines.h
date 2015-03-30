ac9daba61394e20cb3e7a96514312f7dccbdda1a
Hyesu <hotspam0529@gmail.com>
2015-03-30 15:28:04 +0900

    get Button Value

---
+++ src/Button/defines.h
@@ -0,0 +1,20 @@
+/**************************************************
+*
+*   CarTalk System
+*   
+*   Kookmin University Capstone Project
+*   Team. Carcar5talk
+*   20123381 Shin Hyesu
+*
+**************************************************/
+#ifndef __DEFINES_H__
+#define __DEFINES_H__
+
+#include <stdio.h>
+#include <wiringPi.h>
+
+////////////////////////////////////////
+// constants for getting button value
+////////////////////////////////////////
+#define BUTTON	11
+#define DELAY	500
