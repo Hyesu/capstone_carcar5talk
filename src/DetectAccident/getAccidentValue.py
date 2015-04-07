c33c294728d63b390c169957613e1abeb1d7b8dc
Hyesu <hotspam0529@gmail.com>
2015-04-07 13:46:00 +0900

    Detect Accident Module(merge Button module and TriAxis module)

--- src/DetectAccident/getAccidentValue.py
+++ src/DetectAccident/getAccidentValue.py
@@ -11,16 +11,21 @@
 import spidev, os, time
 import math
 import RPi.GPIO as GPIO
+import thread
 
 #################### Constants ####################
+X_CHANNEL = 0
+Y_CHANNEL = 1
+Z_CHANNEL = 2
+LED_YELLOW = 18
+BUTTON = 7
+
+DELAY = 1
+
 COEFF = [0.001861, 0.004444, 0.007667]
 CONST = [1.65, 1.65, 2.31]
 MIN = [0, -180, -90]
 MAX = [360, 180, 90]
-
-X_CHANNEL = 0
-Y_CHANNEL = 1
-Z_CHANNEL = 2
 
 POWER_VOLT = 3.3
 CONV_CONST = 1023
@@ -28,20 +33,24 @@ CONV_CONST = 1023
 COLLISION_THR = 10
 ROLLOVER_THR = 90
 ROLLOVER_MAX = 180
+
 COLLISION_COEF = 0.19
 ROLLOVER_COEF = 45.535
 
-DELAY = 0.5
-
-LED_YELLOW = 18
 
 ###################### Functions ##################
 def init():
 	spi = spidev.SpiDev()
 	spi.open(0,0)
+
+	GPIO.setwarnings(False)
 	
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(LED_YELLOW, GPIO.OUT)
+	GPIO.setup(BUTTON, GPIO.IN, GPIO.PUD_UP)
+
+	GPIO.output(LED_YELLOW, False)
+
 	return spi
 
 def readChannel(spi, channel):
@@ -57,17 +66,23 @@ def getValue(data, axis):
 		value = MAX[axis]
 	return value
 
+def LEDThread():
+	GPIO.output(LED_YELLOW, True)
+	time.sleep(DELAY)
+	GPIO.output(LED_YELLOW, False)
+
 def isCollision(rVector, mVector):
 	aVector = [mVector[0]-rVector[0], mVector[1]-rVector[1], mVector[2]-rVector[2]]
 	aCrash = pow(aVector[0], 2) + pow(aVector[1], 2) + pow(aVector[2], 2)
 	aCrash = math.sqrt(aCrash)
 	
-	if aCrash <= COLLISION_THR:
+	if aCrash >= -COLLISION_THR and aCrash <= COLLISION_THR:
 		return False
 	else:
-		GPIO.output(LED_YELLOW, True)
-		time.sleep(DELAY)
-		GPIO.output(LED_YELLOW, False)
+		try:
+			thread.start_new_thread(LEDThread, ())
+		except:
+			print "Error: unable to start LED Thread"
 		return True
 
 def isRollOver(rVector, mVector):
@@ -82,33 +97,57 @@ def isRollOver(rVector, mVector):
 	theta = math.acos(exp4)
 
 	if theta >= ROLLOVER_THR and theta <= ROLLOVER_MAX:
-		GPIO.output(LED_YELLOW, True)
-		time.sleep(DELAY)
-		GPIO.output(LED_YELLOW, False)
+		try:
+			thread.start_new_thread(LEDThread, ())
+		except:
+			print "Error: unable to start LED Thread"
 		return True
 	else:
 		return False
 
+def isPushed():
+	if not GPIO.input(BUTTON):
+		try:
+			thread.start_new_thread(LEDThread, ())
+		except:
+			print "Error: unable to start LED Thread"
+
+def TriAxisThread(spi):
+	rVector = []
+	while True:
+		xData = readChannel(spi, X_CHANNEL)
+		yData = readChannel(spi, Y_CHANNEL)
+		zData = readChannel(spi, Z_CHANNEL)
+
+		xValue = getValue(xData, X_CHANNEL)
+		yValue = getValue(yData, Y_CHANNEL)
+		zValue = getValue(zData, Z_CHANNEL)
+
+		if rVector == []:
+			rVector = [xValue, yValue, zValue]
+			pass
+
+		mVector = [xValue, yValue, zValue]
+		print("isCollision: {}".format(isCollision(rVector, mVector)))
+		print("isRollOver: {}\n".format(isRollOver(rVector, mVector)))
+
+		time.sleep(DELAY/2)
+
+def ButtonThread():
+	while True:
+		isPushed()
+
 	
 ####################### Main ######################
 spi = init()
-rVector = []
-while True:
-	xData = readChannel(spi, X_CHANNEL)
-	yData = readChannel(spi, Y_CHANNEL)
-	zData = readChannel(spi, Z_CHANNEL)
+try:
+	thread.start_new_thread(TriAxisThread, (spi))
+except:
+	print "Error: unable to start TriAxis thread"
 
-	xValue = getValue(xData, X_CHANNEL)
-	yValue = getValue(yData, Y_CHANNEL)
-	zValue = getValue(zData, Z_CHANNEL)
+try:
+	thread.start_new_thread(ButtonThread, ())
+except:
+	print "Error: unable to start Button thread"
 
-	if rVector == []:
-		rVector = [xValue, yValue, zValue]
-		pass
-
-	mVector = [xValue, yValue, zValue]
-	print("isCollision: {}".format(isCollision(rVector, mVector)))
-	print("isRollOver: {}\n".format(isRollOver(rVector, mVector)))
-
-	time.sleep(DELAY)
 GPIO.cleanup()
