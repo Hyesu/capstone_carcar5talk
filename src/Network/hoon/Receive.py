9f00be245dc1aab59f58f8a3cbfd193cb0601e2f
parkhoon <hoon0204@naver.com>
2015-05-20 10:41:46 +0000

    network

--- src/Network/hoon/Receive.py
+++ src/Network/hoon/Receive.py
@@ -1,8 +1,22 @@
+###########################################
+#
+#	CarTalk System
+#	
+#	Kookmin University Capstone Project
+#	Team. Carcar5talk
+#	20103323 Kim Taewook
+#	20103346 Park Hoon
+#
+############################################
+
+
 import threading
 import socket
 import commands
 
+#Create Socket for receive msg
 recvSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#Bind with same network
 recvSock.bind(("",8080))
 
 #Get My IP Address
@@ -14,6 +28,7 @@ while 1:
 	srcAddr = addr[0]
 	srcAddr = srcAddr +" "
 
+	#Only get others information
 	if myIP != srcAddr:
 		print data
 
